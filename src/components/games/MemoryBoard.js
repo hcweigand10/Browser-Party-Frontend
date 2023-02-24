39d94761a5cd0893fd7fa423bcb6f44340ddd511
hcweigand10 <henryweigand10@gmail.com>
2023-02-24 15:47:56 -0800

    fixing memory board

--- src/components/games/MemoryBoard.js
+++ src/components/games/MemoryBoard.js
@@ -1,5 +1,5 @@
 import '../../css/game.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Card from './MemoryCard';
 import Timer from '../Timer'
 import RoundOver from '../RoundOver'
